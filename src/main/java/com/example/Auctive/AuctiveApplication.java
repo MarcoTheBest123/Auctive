3da6188ae8fba7926e4b8d0af1de0690b8453807
Marco Stefani <m.stefani@itsrizzoli.it>
2022-06-15 15:43:45 +0200

    Initial commit

---
+++ src/main/java/com/example/Auctive/AuctiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auctive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Auctive/AuctiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auctive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
